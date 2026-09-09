730fb75c3fb46ef5f4b379f9058c492d3d50e768
pfriesch <pfriesch@users.noreply.github.com>
2026-09-09 14:32:23 +0000

    Deploy sample for cf2c965991ba8cdec809e70572454f57e6b73269 cf2c965991ba8cdec809e70572454f57e6b73269

--- Transpose.Core.js
+++ Transpose.Core.js
@@ -2,7 +2,7 @@
  * Transpose.Translator generated output.
  */
 Transpose.assemblyVersion("Transpose.Core", "1.0.0.0");
-Transpose.Require && Transpose.Require.cacheBust && Transpose.Require.cacheBust("2oo8tb2iqhf");
+Transpose.Require && Transpose.Require.cacheBust && Transpose.Require.cacheBust("2oz9udxi4hu");
 Transpose.assembly("Transpose.Core", function ($asm, globals) {
     "use strict";
 
